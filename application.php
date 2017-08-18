93fde84c8941bdfaa271b53f02befeb7101c950e
Peter Streltsov <streltsov83@yandex.ru>
2017-08-18 17:41:42 +0300

    0.0.09

--- application.php
+++ application.php
@@ -2,6 +2,7 @@
 
 use \Classes\Navigator as Navigator;
 use \Classes\Settings as Settings;
+use \Database\Connection as Connection;
 
 $exectime = microtime(true);
 
@@ -13,8 +14,10 @@ require_once AUTOLOAD;
 require_once COMPOSER_AUTOLOAD;
 
 new Autoload;
+Connection::getInstance();
 
 require_once ROUTES;
+require_once CONNECTIONS;
 
 $settings = Settings::getInstance();
 
@@ -22,12 +25,12 @@ $page = new Navigator;
 
 $page->start();
 
-if (php_sapi_name() === 'cli') {
+/*if (php_sapi_name() === 'cli') {
     echo "Page will not be loaded - you are running from CLI!".PHP_EOL;
     echo "Current user status: ", $settings->descriptor_userstatus.PHP_EOL;
     //print_r(Settings::$autoload);
     print_r(Settings::$log);
-    }
+    }*/
 
 $exectime = round((microtime(true) - $exectime), 3);
 
---
+++ config/connections.php
@@ -0,0 +1,5 @@
+<?php
+
+use \Database\Connection as Connection;
+
+Connection::define('mysql', 'localhost', 'root');

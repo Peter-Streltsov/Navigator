e895fe79d152370dca6c26e93ec8a9ab2d9a86c2
Peter Streltsov <streltsov83@yandex.ru>
2017-11-15 03:29:12 +0300

    0.2.04

--- autoload.php
+++ autoload.php
@@ -1,14 +1,9 @@
 <?php
 
-
-class Autoloader
-{
-    public function __construct()
-    {
-        $this->autoload();
+function __autoload($class) {
+        echo $class.PHP_EOL;
+        $class = strtolower($class);
+        $class = explode('/', $class);
+        $classfile = $class[1].DS.$class[2];
+        require_once ROOT.$classfile;
     }
-
-    private function autoload() {
-        
-    }
-}

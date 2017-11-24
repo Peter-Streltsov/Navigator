e18eb7f30a23616c484eab242f12d45953e786e9
Peter Streltsov <streltsov83@yandex.ru>
2017-11-24 05:20:22 +0300

    0.2.18 / models (users & base) updated

--- models/basemodel.php
+++ models/basemodel.php
@@ -5,9 +5,11 @@ namespace Scientometrics\Models;
 abstract class BaseModel
 {
     protected $fluent;
+    protected $pdo;
     
-    final public function __construct(\FluentPDO $fluent)
+    final public function __construct(\FluentPDO $fluent, \PDO $pdo)
     {
         $this->fluent = $fluent;
+        $this->pdo = $pdo;
     }
 }
--- models/users.php
+++ models/users.php
@@ -25,10 +25,82 @@ class Users extends Models\BaseModel
     {
         $user['userdata'] = $this->fluent->from('authors')->select('id', 'name', 'lastname');
         return $user;
-    }
+    } // end function
 
-    public function adduser(array $parameters)
+
+    // adding new user
+    public function addUser()
+    {
+        $this->pdo->insertInto('authors')->values($this->name, $this->lastname, $this->position, $this->edu);
+    } // end function
+
+    public function deleteUser()
     {
 
     }
+
+    public function findUser()
+    {
+
+    }
+
+
+    /**
+     * setters
+     */
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    } // end function
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    } // end function
+
+    public function setLastname()
+    {
+        $this->lastname = $lastname;
+    } // end function
+
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    } // end function
+
+    public function setEdu($edu)
+    {
+        $this->edu = $edu;
+    }  // end function
+
+    /**
+     * getters
+     */
+
+    public function getId()
+    {
+        return $this->id;
+    } // end function
+
+    public function getName()
+    {
+        return $this->name;
+    } // end function
+
+    public function getLastname()
+    {
+        return $this->lastname;
+    } // end function
+
+    public function getPosition()
+    {
+        return $this->position;
+    } // end function
+
+    public function getEdu()
+    {
+        return $this->edu;
+    } // end function
+
 } // end class

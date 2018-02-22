3a4968afebd81b05f81b21b5784d879f8fb19327
Peter Streltsov <streltsov83@yandex.ru>
2018-02-22 18:25:58 +0300

    0.3.55

--- models/service/schema.php
+++ models/service/schema.php
@@ -54,7 +54,7 @@ class Schema
      */
     private function createAuthors(): void
     {
-        if ($this->pdo->query("CREATE TABLE scientometrics.authors2 (
+        if ($this->pdo->query("CREATE TABLE IF NOT EXISTS scientometrics.authors2 (
             id int(11) NOT NULL AUTO_INCREMENT,
             name varchar(50) NOT NULL,
             secondname varchar(100),
@@ -81,7 +81,7 @@ class Schema
      */
     private function createPositions(): void
     {
-        if ($this->pdo->query("CREATE TABLE scientometrics.positions2 (
+        if ($this->pdo->query("CREATE TABLE IF NOT EXISTS scientometrics.positions2 (
             id int(11) NOT NULL AUTO_INCREMENT,
             `position` varchar(255) NOT NULL,
             `key` varchar(255) NOT NULL,
@@ -105,7 +105,7 @@ class Schema
      */
     private function createArticlesAuthors(): void
     {
-        if ($this->pdo->query("CREATE TABLE scientometrics.articles_authors2 (
+        if ($this->pdo->query("CREATE TABLE IF NOT EXISTS scientometrics.articles_authors2 (
             id int(11) NOT NULL AUTO_INCREMENT,
             name varchar(50) DEFAULT NULL,
             lastname varchar(255) DEFAULT NULL,
@@ -128,75 +128,108 @@ class Schema
     /**
      * @return void
      */
-    private function createArticles()
+    private function createArticles(): void
     {
         if ($this->pdo->query("CREATE TABLE IF NOT EXISTS `articles2` 
-                            (`id` int(11) NOT NULL AUTO_INCREMENT,
-                            `title` varchar(30) NOT NULL,
-                            `subtitle` varchar(30) DEFAULT NULL,
-                            'magazine' varchar(250),
-                            `author` int(11) NOT NULL,
-                            `year` int(11),
-                            'first_page' text,
-                            'last_page' text,
-                            'DOI' varchar(250),
-                            'country' varchar(250),
-                            'annotation' text,
-                            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;"))
+            (`id` int(11) NOT NULL AUTO_INCREMENT,
+            `title` varchar(30) NOT NULL,
+            `subtitle` varchar(30) DEFAULT NULL,
+            'magazine' varchar(250),
+            `author` int(11) NOT NULL,
+            `year` int(11),
+            'first_page' text,
+            'last_page' text,
+            'DOI' varchar(250),
+            'country' varchar(250),
+            'annotation' text,
+            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;"))
         {
             echo "таблица 'articles' создана;<br>";
         } else {
             echo "не получилось создать таблицу 'articles';<br>";
         }
+
     } // end function
 
 
+    /**
+     * @return void
+     */
     private function createMonographies(): void
     {
         if ($this->pdo->query("CREATE TABLE IF NOT EXISTS `monographies2` 
-                            (`id` int(11) NOT NULL AUTO_INCREMENT,
-                            `title` varchar(30) NOT NULL,
-                            `subtitle` varchar(30),
-                            `author` int(11) NOT NULL,
-                            `year` int(11),
-                            `publisher` varchar(100),
-                            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;")) 
-                            {
-                                echo "таблица 'monographies' создана;<br>";
-                            } else {
-                                echo "не получилось создать таблицу 'monographies';<br>";
-                            }
+            (`id` int(11) NOT NULL AUTO_INCREMENT,
+            `title` varchar(30) NOT NULL,
+            `subtitle` varchar(30),
+            `author` int(11) NOT NULL,
+            `year` int(11),
+            `publisher` varchar(100),
+            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;")) 
+        {
+            echo "таблица 'monographies' создана;<br>";
+        } else {
+            echo "не получилось создать таблицу 'monographies';<br>";
+        }
+
     } // end function
 
     private function createReports(): void
     {
         if ($this->pdo->query("CREATE TABLE IF NOT EXISTS `reports2` 
-                            (`id` int(11) NOT NULL AUTO_INCREMENT,
-                            `title` varchar(30) NOT NULL,
-                            `subtitle` varchar(30),
-                            `author` int(11) NOT NULL,
-                            `year` int(11),
-                            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;")) 
-                            {
-                                echo "таблица 'reports' создана;<br>";
-                            } else {
-                                echo "не получилось создать таблицу 'reports';<br>";
-                    }
+            (`id` int(11) NOT NULL AUTO_INCREMENT,
+            `title` varchar(30) NOT NULL,
+            `subtitle` varchar(30),
+            `author` int(11) NOT NULL,
+            `year` int(11),
+            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;")) 
+        {
+            echo "таблица 'reports' создана;<br>";
+        } else {
+            echo "не получилось создать таблицу 'reports';<br>";
+        }
+    
     } // end function
 
-    private function createConferencies()
+    /**
+     * @return void
+     */
+    private function createConferencies(): void
     {
         if ($this->pdo->query("CREATE TABLE IF NOT EXISTS `conferencies2` 
-                            (`id` int(11) NOT NULL AUTO_INCREMENT,
-                            `title` varchar(30) NOT NULL,
-                            `subtitle` varchar(30),
-                            `author` int(11) NOT NULL,
-                            `year` int(11),
-                            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;")) 
-                            {
-                                echo "таблица 'conferencies' создана;<br>";
-                            } else {
-                                echo "не получилось создать таблицу 'conferencies';<br>";
-                            }
+            (`id` int(11) NOT NULL AUTO_INCREMENT,
+            `title` varchar(30) NOT NULL,
+            `subtitle` varchar(30),
+            `author` int(11) NOT NULL,
+            `year` int(11),
+            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;")) 
+        {
+            echo "таблица 'conferencies' создана;<br>";
+        } else {
+            echo "не получилось создать таблицу 'conferencies';<br>";
+        }
+    
     } // end function
-}
+
+    /**
+     * @return void
+     */
+    private function createIndexesArticles(): void
+    {
+        if ($this->pdo->query("CREATE TABLE IF NOT EXISTS `indexes_articles` 
+            (`id` int(11) NOT NULL AUTO_INCREMENT,
+            `article_index` varchar(250) NOT NULL,
+            `value` int(11) NOT NULL,
+            PRIMARY KEY (`id`)) Engine=InnoDB DEFAULT CHARSET=utf8;")) 
+            {
+                echo "таблица 'indexes_articles' создана;<br>";
+            } else {
+                echo "не получилось создать таблицу 'indexes_articles';<br>";
+            }          
+    } // end function
+    
+    private function createIndexes()
+    {
+
+    }
+
+} // end class

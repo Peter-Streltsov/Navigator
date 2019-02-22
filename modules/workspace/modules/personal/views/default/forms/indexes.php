bdede1f24eaaa958aa71af5f380e00fc771da1fd
Peter Streltsov <streltsov83@yandex.ru>
2019-02-22 23:20:08 +0300

    0.4.152: personal page updated (indexes form, added modal window);

--- modules/workspace/modules/personal/views/default/forms/indexes.php
+++ modules/workspace/modules/personal/views/default/forms/indexes.php
@@ -11,21 +11,38 @@ $alert = '<div style="border-radius: 2pc; background-color: lightcoral;">#</div>
 <div class="panel panel-default">
     <div class="panel panel-body">
         <div class="row">
-            <div class="col-lg-5">
+            <div class="col-lg-4">
                 <b style="color: gray;">Индекс <?= $index; ?></b>
             </div>
             <div class="col-lg-5">
                 <b style="color: gray;"><?= $median; ?></b>
             </div>
-            <div class="col-lg-2">
+            <div class="col-lg-3">
                 <?php
                 if ($index > $median * 0.6) {
-                    echo $success;
+                    //echo $success;
+                    $color = 'lightgreen';
+                    $message = 'Ваш индекс выше среднего';
                 } elseif ($index <= $median * 0.6 && $index > $median * 0.3) {
-                    echo $warning;
+                    //echo $warning;
+                    $color = 'lightgoldenyellow';
+                    $message = 'Ваш индекс находится в средней группе';
                 } else {
-                    echo $alert;
+                    //echo $alert;
+                    $color = 'lightcoral';
+                    $message = 'Ваш индекс ниже значений средней группы';
                 }
+                \yii\bootstrap\Modal::begin([
+                        'toggleButton' => [
+                            'class' => 'btn btn-success',
+                            'style' => "color: $color;",
+                            'label' => '<span class="glyphicon glyphicon-question-sign"></span>'
+                        ]
+                ]);
+                echo "<br><br>";
+                echo $message;
+                echo "<br><br><br>";
+                \yii\bootstrap\Modal::end();
                 ?>
             </div>
         </div>
